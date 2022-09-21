03f3e2e029e016ef377488c092a4c1ad6ad9d001
Anonno Ruddho <ahmedrimon377472@gmail.com>
2022-09-21 16:56:00 +0600

    half complete hero

---
+++ src/component/Hero/Hero.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Hero = () => {
+     return (
+          <div className="bg-[url('../../images/backgroundHero.jpg')]">
+               <h2 className="font-bigilla text-8xl">I CAM</h2>
+          </div>
+     );
+};
+
+export default Hero;
--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Hero from '../Hero/Hero.js';
 
 const Home = () => {
      return (
           <div className="bg-[#CACABA] h-screen">
-
+               <Hero />
           </div>
      );
 };
